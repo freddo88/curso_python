b53c84d4ddcddc391584383eee36ae2ea3009495
Fred <fsolisvi@gmail.com>
2025-03-19 23:54:07 -0500

    aplicaion de calculadora sencilla

---
+++ tipos/calculadora.py
@@ -0,0 +1,16 @@
+n1 = int(input("Ingresa primer numero "))
+n2 = int(input("Ingresa segundo numero "))
+
+suma = n1 + n2
+resta = n1 - n2
+multi = n1 * n2
+divi = n1 / n2
+
+mensaje = f"""Para los numeros {n1} y {n2},
+el resultado de la suma es {suma}
+el resultado de la resta es {resta}
+el resultado de la multiplicacion es {multi}
+el resultado de la division es {divi}
+"""
+
+print(mensaje)
