df103395ac2d22163b73e8ac9473c7fdf801815e
Fred <fsolisvi@gmail.com>
2025-03-28 22:36:44 -0500

    Return y alcance

---
+++ funciones/04-return.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    resultado = a + b
+    # retornamos el valor de resultado para que sea asignado en el llamado de la funcion
+    return resultado
+
+
+c = suma(5, 6)
+d = suma(c, 3)
+
+print(d)
