03a07609b8a3d15dcbe658c0e457641558e2ac54
Fred <fsolisvi@gmail.com>
2025-04-01 00:07:28 -0500

    datos avanzados listas

---
+++ tipos-datos-avanzados/01-listas.py
@@ -0,0 +1,10 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["cocinero", "señora", "profesor"]
+booleans = [True, False, True, True]
+matrix = [[0, 1], [1, 0]]
+ceros = [0]*10  # listado de 10 ceros
+alfanumerico = numeros+letras  # juntamos dos listas
+rango = list(range(10))  # crear una lista del 1 al 10
+string = list("Hola mundo")
+print(string)
