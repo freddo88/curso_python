8e84aa33c817d832c74b482c271462993d150540
Fred <fsolisvi@gmail.com>
2025-04-01 00:34:03 -0500

    manipulacion de listas

---
+++ tipos-datos-avanzados/02-manipulando-listas.py
@@ -0,0 +1,7 @@
+mascotas = ["Firulais", "Santa", "Luna", "Miel"]
+print(mascotas[1])  # acedemos a un elemento de la lista
+mascotas[1] = "Copito"  # cambiamos un elemento de la lista
+print(mascotas)
+print(mascotas[0:2])  # imprimimos un parcial de la lista
+print(mascotas[-1])  # imprimimos el ultimo elemento de la lista
+print(mascotas[::2])  # imprime los elementos de dos en dos
---
+++ tipos-datos-avanzados/03-desempaquetar-lista.py
@@ -0,0 +1,4 @@
+numeros = [1, 2, 3]
+# le asigno cada variable a cada elemento de la lista
+primero, segundo, tercero = numeros
+print(primero, segundo, tercero)
