7bcb70ecda7b767f62ccb6f84c9eeffb558605ee
Fred <fsolisvi@gmail.com>
2025-04-01 23:22:18 -0500

    iterando y contandos elementos de una lista

---
+++ tipos-datos-avanzados/04-iterar-listas.py
@@ -0,0 +1,5 @@
+mascotas = ["Carlos", "Jose", "Juan", "Manuel"]
+
+# convierte la lista en una tupla enumerada
+for indice, mascota in enumerate(mascotas):
+    print(indice, mascota)
---
+++ tipos-datos-avanzados/05-buscar-elemento.py
@@ -0,0 +1,6 @@
+nombres = ["Carlos", "Jose", "Juan", "Manuel", "Jose"]
+
+# buscamos el indice donde se encuentra el elemento
+print(nombres.index("Jose"))
+# contamos el numero de veces se repite un elemento
+print(nombres.count("Jose"))
