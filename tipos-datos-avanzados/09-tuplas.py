7a1fa3e72c0dbab0ced6e82b7cfe5412a62c0050
Fred <fsolisvi@gmail.com>
2025-04-07 23:53:13 -0500

    tuplas

---
+++ tipos-datos-avanzados/09-tuplas.py
@@ -0,0 +1,6 @@
+# las tuplas tiene el misn¿mo comportamiento que una listas
+# pero no se pueden modificar
+numeros = (1, 2, 3, 4) + (7, 8, 9)
+print(numeros)
+
+punto = tuple([1, 2])  # metodo que convierte lementos a tupla
